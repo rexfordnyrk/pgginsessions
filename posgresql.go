7d18a4c4ff45e368c50d97f26d87b775665d708b
Rexford Nyarko <rexfordnyrk@gmail.com>
2020-01-22 16:25:06 +0000

    implemented pg backend(no tests written);

---
+++ posgresql.go
@@ -0,0 +1,39 @@
+package pgginsessions
+
+import (
+	"database/sql"
+	"github.com/antonlindstrom/pgstore"
+	"github.com/gin-contrib/sessions"
+)
+
+type Store interface {
+	sessions.Store
+}
+
+func NewStore(dbURL string, keyPairs ...[]byte) (Store, error) {
+	npgstore, err := pgstore.NewPGStore(dbURL, keyPairs...)
+	if err != nil {
+		return nil, err
+	}
+	return &store{npgstore}, nil
+}
+
+// NewPGStoreFromPool creates a new PGStore instance from an existing
+// database/sql pool.
+func NewPGStoreFromPool(db *sql.DB, keyPairs ...[]byte) (Store, error) {
+
+	dbStore, err := pgstore.NewPGStoreFromPool(db, keyPairs...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &store{dbStore}, nil
+}
+
+type store struct {
+	*pgstore.PGStore
+}
+
+func (c *store) Options(options sessions.Options) {
+	c.PGStore.Options = options.ToGorillaOptions()
+}
