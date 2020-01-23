0515253d74ee385e3eef29003fe5e704bd1cea09
Rexford Nyarko <rexfordnyrk@gmail.com>
2020-01-23 11:36:00 +0000

    creste store with custom schema name and exisiting db

--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module github.com/rexfordnyrk/pgginsessions
 go 1.12
 
 require (
-	github.com/antonlindstrom/pgstore v0.0.0-20170604072116-a407030ba6d0
 	github.com/gin-contrib/sessions v0.0.3
-	github.com/gorilla/sessions v1.2.0 // indirect
-	github.com/lib/pq v1.3.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rexfordnyrk/pgstore v0.0.0-20200123065856-b7c43afb0476
 )
--- posgresql.go
+++ posgresql.go
@@ -2,7 +2,7 @@ package pgginsessions
 
 import (
 	"database/sql"
-	"github.com/antonlindstrom/pgstore"
+	"github.com/rexfordnyrk/pgstore"
 	"github.com/gin-contrib/sessions"
 )
 
@@ -18,11 +18,31 @@ func NewStore(dbURL string, keyPairs ...[]byte) (Store, error) {
 	return &store{npgstore}, nil
 }
 
+func NewPGStoreWithSchema(dbURL string, schema string, keyPairs ...[]byte) (Store, error) {
+	npgstore, err := pgstore.NewPGStoreWithSchema(dbURL, schema, keyPairs...)
+	if err != nil {
+		return nil, err
+	}
+	return &store{npgstore}, nil
+}
+
 // NewPGStoreFromPool creates a new PGStore instance from an existing
 // database/sql pool.
 func NewPGStoreFromPool(db *sql.DB, keyPairs ...[]byte) (Store, error) {
 
 	dbStore, err := pgstore.NewPGStoreFromPool(db, keyPairs...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &store{dbStore}, nil
+}
+
+// NewPGStoreFromPool creates a new PGStore instance from an existing
+// database/sql pool.
+func NewPGStoreFromPoolWithSchema(db *sql.DB, schema string, keyPairs ...[]byte) (Store, error) {
+
+	dbStore, err := pgstore.NewPGStoreFromPoolWithSchema(db, schema, keyPairs...)
 	if err != nil {
 		return nil, err
 	}
